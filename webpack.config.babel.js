5b495560ed7338c0a7d4cddeacdbbccbf1637769
Agustin Castiarena <castiarena@gmail.com>
2017-10-05 19:32:32 -0300

    First commit of code-challenge

---
+++ webpack.config.babel.js
@@ -0,0 +1,30 @@
+import 'babel-polyfill';
+import webpack from 'webpack';
+import path from 'path';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+
+export default {
+    entry: {
+        'code-challenge': './src/app.js',
+        'code-challenge.min': './src/app.js'
+    },
+
+    output:{
+        path: `${__dirname}/build`,
+        filename: 'scripts/[name].js',
+        publicPath: 'build'
+    },
+
+    module:{
+        loaders:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options:{
+                    presets: ['env']
+                }
+            }
+        ]
+    }
+}
